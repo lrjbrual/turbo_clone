3777eef3045d8a17a8e588d180b05699cee2da69
lrjbrual <lrjbrual@live.com>
2022-10-17 13:51:22 +0200

    update good result

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -1,3 +1,5 @@
+require "turbo_clone/test_assertions"
+
 module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
@@ -10,5 +12,26 @@ module TurboClone
         helper TurboClone::Engine.helpers
       end
     end
+
+    # this include the test_assertions
+    initializer "turbo.test_assertions" do 
+      ActiveSupport.on_load :active_support_test_case do 
+        include TurboClone::TestAssertions
+      end
+    end
+    
+    initializer "turbo.integration_test_request_encoding" do 
+      ActiveSupport.on_load :action_dispatch_integration_test do 
+        class ActionDispatch::RequestEncoder
+          class TurboStreamEncoder < IdentityEncoder
+            header = [ Mime[:turbo_stream], Mime[:html] ].join(",")
+            define_method(:accept_header) { header }
+          end
+
+          @encoders[:turbo_stream] = TurboStreamEncoder.new
+        end
+      end
+    end
+
   end
 end
